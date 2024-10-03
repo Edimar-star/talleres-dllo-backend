e47df106e49461b72d0de63905c703d97aeef996
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2024-10-03 13:26:28 -0500

    Delete taller-04/src/user/v1/read.user.byFaction.ts

--- taller-04/src/user/v1/read.user.byFaction.ts
+++
@@ -1,8 +0,0 @@
-import { users, UserType } from "./user.model";
-
-function readUserByFaction(faction: string): UserType[] {
-    const result : UserType[] = users.filter(user => user.faction === faction);
-    return result;
-}
-
-export default readUserByFaction;
