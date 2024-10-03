b4f7078c06a2b84742a61e6618a07a6c714270e5
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2024-10-03 13:26:38 -0500

    Delete taller-04/src/user/v1/read.user.byHobby.ts

--- taller-04/src/user/v1/read.user.byHobby.ts
+++
@@ -1,8 +0,0 @@
-import { users, UserType } from "./user.model";
-
-function readUserByHobby(hobby: string): UserType[] {
-  const results : UserType[] = users.filter(user => user.hobbies.includes(hobby));
-  return results;
-}
-
-export default readUserByHobby;
