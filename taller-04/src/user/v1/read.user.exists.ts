40ed2260900c5de35fa34acb3524d148f58759d2
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2024-10-03 13:26:51 -0500

    Delete taller-04/src/user/v1/read.user.exists.ts

--- taller-04/src/user/v1/read.user.exists.ts
+++
@@ -1,8 +0,0 @@
-import { users } from "./user.model";
-
-function readUserExists(id: number): boolean {
-    const result : boolean = users.some(user => user.id === id);
-    return result;
-}
-
-export default readUserExists;
